0d7e7ca1a3e69cb960dd25ddc77488fd4f7cb127
warddav16 <warddav16@gmail.com>
2016-01-23 14:08:42 -0600

    files

---
+++ Assets/Scripts/PlayerEntity.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEntity : MonoBehaviour
+{
+	void Start ()
+    {
+        GameManager.instance.CmdRegisterPlayer(UserId.GetId());
+    }
+}
---
+++ Assets/Scripts/UserId.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserId
+{
+    private static string thisId = null;
+
+    public static string GetId()
+    {
+        if (thisId == null)
+        {
+            thisId = Random.Range(0, 10000001).ToString(); // TODO: Replace with better id (from steam!)
+        }
+
+        return thisId;
+    }
+}
